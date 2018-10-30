e0a96a1725c2cf623e3bcdc13e4ec16a7b1491f0
hoaanhvu <44601153+hoaanhvu@users.noreply.github.com>
2018-10-30 16:28:04 +0700

    Update app/Http/Controllers/BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -238,7 +238,11 @@ class BookController extends Controller
     public function getListBook()
     {
         $book =bt_book::all()->take(8);
+        if($book->count() == 0)
+            $book = null;
         $LNewsbooks = bt_book::orderBy('book_id','desc')->take(8)->get();
+        if($LNewsbooks->count() == 0)
+            $LNewsbooks = null;
         $rating = DB::table('bt_rates')->select(DB::raw('book_id,AVG(book_rating) as rating'))->groupBy('book_id')->get();
         return view('webclient.index',['Books'=>$book,'LNewsBook'=>$LNewsbooks,'Rating'=>$rating]);
     }
