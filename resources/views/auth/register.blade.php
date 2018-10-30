74910e478c7120404a7c8eb81fb48be7e82e08fd
hoaanhvu <44601153+hoaanhvu@users.noreply.github.com>
2018-10-30 17:25:29 +0700

    Update resources/views/auth/register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,15 +1,8 @@
-<!--
-Author: W3layouts
-Author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
--->
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Best Store a Ecommerce Online Shopping Category Flat Bootstrap Responsive Website Template | Register ::
-        w3layouts</title>
-    <!-- for-mobile-apps -->
+@extends('webclient.layout')
+@section('title')
+    Đăng kí
+@endsection
+@section('css')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="keywords" content="Best Store Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
@@ -45,284 +38,48 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
         new WOW().init();
     </script>
     <!-- //animation-effect -->
-</head>
+@endsection
+@section('content')
+    <!-- breadcrumbs -->
+    <div class="breadcrumbs">
+        <div class="container">
+            <ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
+                <li><a href="trangchu"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Trang chủ</a></li>
+                <li class="active">Đăng ký</li>
+            </ol>
+        </div>
+    </div>
+    <!-- //breadcrumbs -->
+    <!-- register -->
+    <div class="register">
+        <div class="container">
+            <h3 class="animated wow zoomIn" data-wow-delay=".5s">Đăng ký tại đây</h3>
+            <p class="est animated wow zoomIn" data-wow-delay=".5s">Quý khách vui lòng đăng ký tài khoản tại đây để có thể
+                đăng nhập sử dụng dịch vụ của hệ thống</p>
+            <div class="login-form-grids">
+                <h5 classưe="animated wow slideInUp">Thông tin khách hàng</h5>
+                @if(session('status'))
+                    <ul>
+                        <li class="text-danger">
+                            {{session('status')}}
+                        </li>
+                    </ul>
+                @endif
+                <form class="animated wow slideInUp" role="form" method="POST" action="{{route('register')}}">
+                    @csrf
+                    <input type="text" placeholder="FName..." required=" " name="user_fname">
+                    <input type="text" placeholder="User Name..." required=" "name="user_name">
+                    <h6 class="animated wow slideInUp">Thông tin đăng nhập</h6>
+                    <input type="email" placeholder="Địa chỉ Email" required=" " name="user_email">
+                    <input type="password" placeholder="Mật khẩu" required=" " name="password" id="password">
+                    <input type="password" placeholder="Xác nhận mật khẩu" required=" " name="password_confirmation" id="confirmpassword">
+                    <input type="submit" value="Đăng ký" id="register">
+                </form>
+            </div>
+            <div class="register-home animated wow slideInUp">
+                <a href="/">Trang chủ</a>
+            </div>
+        </div>
+    </div>
 
-<body>
-<!-- header -->
-<div class="header">
-    <div class="container">
-        <div class="header-grid">
-            <div class="header-grid-left animated wow slideInLeft" data-wow-delay=".5s">
-                <ul>
-                    <li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a
-                            href="mailto:info@example.com">phamdanghieu47@gmail.com</a></li>
-                    <li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>+84961098497</li>
-                    <li><i class="glyphicon glyphicon-log-in" aria-hidden="true"></i><a href="login.blade.php">Đăng Nhập</a>
-                    </li>
-                    <li><i class="glyphicon glyphicon-book" aria-hidden="true"></i><a href="register.blade.php">Đăng Ký</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="header-grid-right animated wow slideInRight" data-wow-delay=".5s">
-                <ul class="social-icons">
-                    <li><a href="#" class="facebook"></a></li>
-                    <li><a href="#" class="twitter"></a></li>
-                    <li><a href="#" class="g"></a></li>
-                    <li><a href="#" class="instagram"></a></li>
-                </ul>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div class="logo-nav">
-            <div class="logo-nav-left animated wow zoomIn" data-wow-delay=".5s">
-                <h1><a href="trangchu.blade.php">Book Store <span>Shop anywhere</span></a></h1>
-            </div>
-            <div class="logo-nav-left1">
-                <nav class="navbar navbar-default">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                    <div class="navbar-header nav_2">
-                        <button type="button" class="navbar-toggle collapsed navbar-toggle1" data-toggle="collapse"
-                                data-target="#bs-megadropdown-tabs">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                    </div>
-                    <div class="collapse navbar-collapse" id="bs-megadropdown-tabs">
-                        <ul class="nav navbar-nav">
-                            <li class="active"><a href="trangchu.blade.php" class="act">Trang Chủ</a></li>
-                            <!-- Mega Menu -->
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Danh Mục Sách <b
-                                        class="caret"></b></a>
-                                <ul class="dropdown-menu multi-column columns-3">
-                                    <div class="row">
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Sách Trong Nước</h6>
-                                                <li><a href="products.blade.php">Sách Giáo Khoa</a></li>
-                                                <li><a href="products.blade.php">Sách Thiếu Nhi</a></li>
-                                                <li><a href="products.blade.php">Sách Văn Học Trong Nước</a></li>
-                                                <li><a href="products.blade.php">Sách Kinh Tế</a></li>
-                                                <li><a href="products.blade.php">Sách Thưởng Thức - Đời Sống</a></li>
-                                                <li><a href="products.blade.php">Sách Tin Học - Ngoại Ngữ</a></li>
-                                                <li><a href="products.blade.php">Sách Chuyên Ngành</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Sách Nước Ngoài </h6>
-                                                <li><a href="products.blade.php">Literature & Fiction</a></li>
-                                                <li><a href="products.blade.php">Travel</a></li>
-                                                <li><a href="products.blade.php">Children's Book</a></li>
-                                                <li><a href="products.blade.php">English as A Second Language</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Tác Giả Nổi Bật</h6>
-                                                <li><a href="products.blade.php">Clothing</a></li>
-                                                <li><a href="products.blade.php">Wallets,Bags</a></li>
-                                                <li><a href="products.blade.php">Footwear</a></li>
-                                                <li><a href="products.blade.php">Watches</a></li>
-                                                <li><a href="products.blade.php">Accessories</a></li>
-                                                <li><a href="products.blade.php">Jewellery</a></li>
-                                                <li><a href="products.blade.php">Beauty & Grooming</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </ul>
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Nhà Xuất Bản<b
-                                        class="caret"></b></a>
-                                <ul class="dropdown-menu multi-column columns-3">
-                                    <div class="row">
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Nhà xuất bản trong nước</h6>
-                                                <li><a href="furniture.blade.php">Cookware</a></li>
-                                                <li><a href="furniture.blade.php">Sofas</a></li>
-                                                <li><a href="furniture.blade.php">Dining Tables</a></li>
-                                                <li><a href="furniture.blade.php">Shoe Racks</a></li>
-                                                <li><a href="furniture.blade.php">Home Decor</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Nhà xuất bản nước ngoài</h6>
-                                                <li><a href="furniture.blade.php">Carpets</a></li>
-                                                <li><a href="furniture.blade.php">Tables</a></li>
-                                                <li><a href="furniture.blade.php">Sofas</a></li>
-                                                <li><a href="furniture.blade.php">Shoe Racks</a></li>
-                                                <li><a href="furniture.blade.php">Sockets</a></li>
-                                                <li><a href="furniture.blade.php">Electrical Machines</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="col-sm-4">
-                                            <ul class="multi-column-dropdown">
-                                                <h6>Decorations</h6>
-                                                <li><a href="furniture.blade.php">Toys</a></li>
-                                                <li><a href="furniture.blade.php">Wall Clock</a></li>
-                                                <li><a href="furniture.blade.php">Lighting</a></li>
-                                                <li><a href="furniture.blade.php">Top Brands</a></li>
-                                            </ul>
-                                        </div>
-                                        <div class="clearfix"></div>
-                                    </div>
-                                </ul>
-                            </li>
-                            <li><a href="mail.blade.php">Liên hệ với chúng tôi</a></li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-            <div class="logo-nav-right">
-                <div class="search-box">
-                    <div id="sb-search" class="sb-search">
-                        <form>
-                            <input class="sb-search-input" placeholder="Nhập từ khóa để tìm kiếm..." type="search"
-                                   id="search">
-                            <input class="sb-search-submit" type="submit" value="">
-                            <span class="sb-icon-search"> </span>
-                        </form>
-                    </div>
-                </div>
-                <!-- search-scripts -->
-                <script src="js/classie.js"></script>
-                <script src="js/uisearch.js"></script>
-                <script>
-                    new UISearch(document.getElementById('sb-search'));
-                </script>
-                <!-- //search-scripts -->
-            </div>
-            <div class="header-right">
-                <div class="cart box_1">
-                    <a href="checkout.blade.php">
-                        <h3>
-                            <div class="total">
-                                <span class="simpleCart_total"></span> (<span id="simpleCart_quantity"
-                                                                              class="simpleCart_quantity"></span> sản
-                                phẩm)
-                            </div>
-                            <img src="images/bag.png" alt=""/>
-                        </h3>
-                    </a>
-                    <p><a href="javascript:;" class="simpleCart_empty">Giỏ hàng rỗng</a></p>
-                    <div class="clearfix"></div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-    </div>
-</div>
-<!-- //header -->
-<!-- breadcrumbs -->
-<div class="breadcrumbs">
-    <div class="container">
-        <ol class="breadcrumb breadcrumb1 animated wow slideInLeft" data-wow-delay=".5s">
-            <li><a href="trangchu"><span class="glyphicon glyphicon-home" aria-hidden="true"></span>Trang chủ</a></li>
-            <li class="active">Đăng ký</li>
-        </ol>
-    </div>
-</div>
-<!-- //breadcrumbs -->
-<!-- register -->
-<div class="register">
-    <div class="container">
-        <h3 class="animated wow zoomIn" data-wow-delay=".5s">Đăng ký tại đây</h3>
-        <p class="est animated wow zoomIn" data-wow-delay=".5s">Quý khách vui lòng đăng ký tài khoản tại đây để có thể
-            đăng nhập sử dụng dịch vụ của hệ thống</p>
-        <div class="login-form-grids">
-            <h5 classưe="animated wow slideInUp">Thông tin khách hàng</h5>
-            @if(session('status'))
-                <ul>
-                    <li class="text-danger">
-                        {{session('status')}}
-                    </li>
-                </ul>
-            @endif
-            <form class="animated wow slideInUp" role="form" method="POST" action="{{route('register')}}">
-                @csrf
-                <input type="text" placeholder="FName..." required=" " name="user_fname">
-                <input type="text" placeholder="User Name..." required=" "name="user_name">
-                <h6 class="animated wow slideInUp">Thông tin đăng nhập</h6>
-                <input type="email" placeholder="Địa chỉ Email" required=" " name="user_email">
-                <input type="password" placeholder="Mật khẩu" required=" " name="password" id="password">
-                <input type="password" placeholder="Xác nhận mật khẩu" required=" " name="password_confirmation" id="confirmpassword">
-                <input type="submit" value="Đăng ký" id="register">
-            </form>
-        </div>
-        <div class="register-home animated wow slideInUp">
-            <a href="trangchu">Trang chủ</a>
-        </div>
-    </div>
-</div>
-<!-- //register -->
-<!-- footer -->
-<div class="footer">
-    <div class="container">
-        <div class="footer-grids">
-            <div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".5s">
-                <h3>Thông tin</h3>
-                <p><b style="font-size: medium">Nhóm phát triển gồm</b><br>
-                    <a href="https://www.facebook.com/phamdanghieu0407">Phạm Đăng Hiếu</a><br>
-                    <a href="https://www.facebook.com/phamdanghieu0407">Vũ Văn Đức</a><br>
-                    <a href="https://www.facebook.com/phamdanghieu0407">Nguyễn Thị Hồng Ngọc</a><br>
-                    Lớp: KTPM1-K10
-                </p>
-            </div>
-            <div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".6s">
-                <h3>Liên Hệ</h3>
-                <ul>
-                    <li><i class="glyphicon glyphicon-map-marker" aria-hidden="true"></i>Đại Học Công Nghiệp Hà
-                        Nội<span>Bắc Từ Liêm , Hà Nội.</span>
-                    </li>
-                    <li><i class="glyphicon glyphicon-envelope" aria-hidden="true"></i><a
-                            href="mailto:phamdanghieu47@gmail.com">phamdanghieu@gmail.com</a></li>
-                    <li><i class="glyphicon glyphicon-earphone" aria-hidden="true"></i>+84961098497</li>
-                </ul>
-            </div>
-            <div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".7s">
-                <h3>Thanh Toán</h3>
-                <div class="footer-grid-left">
-                    <a><img src="images/13.jpg" alt=" " class="img-responsive"/></a><br>
-                    <p>Thanh toán khi nhận hàng</p>
-                </div>
-                <div class="footer-grid-left">
-                    <a><img src="images/14.jpg" alt=" " class="img-responsive"/></a><br>
-                    <p>Thanh toán qua thẻ ngân hàng nội địa</p>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="col-md-3 footer-grid animated wow slideInLeft" data-wow-delay=".7s">
-                <h3>Follow Us</h3>
-                <div class="footer-grid-left">
-                    <a href="https://www.facebook.com/"><img src="images/icon_fb_2x.jpg" alt=" "
-                                                             class="img-responsive"/></a><br>
-                </div>
-                <div class="footer-grid-left">
-                    <a href="https://plus.google.com"><img src="images/icon_g_2x.jpg" alt=" "
-                                                           class="img-responsive"/></a><br>
-                </div>
-                <div class="footer-grid-left">
-                    <a href="https://twitter.com/"><img src="images/icon_twt_2x.jpg" alt=" "
-                                                        class="img-responsive"/></a><br>
-                </div>
-                <div class="clearfix"></div>
-            </div>
-            <div class="clearfix"></div>
-        </div>
-        <div class="footer-logo animated wow slideInUp" data-wow-delay=".5s">
-            <h2><a href="trangchu.blade.php">Book Store <span>shop anywhere</span></a></h2>
-        </div>
-        <div class="copy-right animated wow slideInUp" data-wow-delay=".5s">
-            <p>&copy 2016 Book Store. All rights reserved | Design by Nhom 7</a></p>
-        </div>
-    </div>
-</div>
-<!-- //footer -->
-</body>
-</html>
+@endsection
