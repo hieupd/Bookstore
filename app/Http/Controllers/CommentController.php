dc63b6b7f23d12f3b01336726bf2c33e3ee7cd83
hoaanhvu <44601153+hoaanhvu@users.noreply.github.com>
2018-11-01 10:40:37 +0700

    Update app/Http/Controllers/CommentController.php

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+use Request;
+use App\bt_comment;
+class CommentController extends Controller
+{
+    //
+    public function postComment($id)
+    {
+        if(Request::ajax())
+        {
+           $cmtcontent = Request::get('cmtcontent');
+           $userid = Request::get('userid');
+           $comment = new bt_comment();
+           $comment->member_id = $userid;
+           $comment->book_id = $id;
+           $comment->comment_content = $cmtcontent;
+           $comment->save();
+           return "Success";
+        }
+    }
+    public function getComment($id)
+    {
+        $cmts = bt_comment::join('users','bt_comments.member_id','=','users.id')->where('book_id',$id)->paginate(3);
+        if($cmts)
+        foreach ($cmts as $cmt)
+        {
+            echo    "
+                                           <div class=\"bootstrap-tab-text-grid-right\">
+												<ul>
+													<li><a href=\"#\">".$cmt->user_fname."</a></li>
+													<li><a href=\"#\"><span class=\"glyphicon glyphicon-share\" aria-hidden=\"true\"></span>Reply</a></li>
+												</ul>
+												<hr>
+												<p >".$cmt->comment_content."</p>
+												<hr>
+											</div> ";
+        }
+    }
+}
