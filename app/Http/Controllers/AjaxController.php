242192c2bff5d91f507b22d4b70397758bcf468c
hoaanhvu <44601153+hoaanhvu@users.noreply.github.com>
2018-11-01 10:40:30 +0700

    Update app/Http/Controllers/AjaxController.php

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,17 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\bt_book;
 use App\bt_type;
 use Illuminate\Http\Request;
 
 class AjaxController extends Controller
 {
-    public function getBooktype($id)
+    public function getBooktype($id, $typeid)
     {
         $booktype = bt_type::where('category_id',$id)->get();
         foreach ($booktype as $tp)
         {
-            echo "<option value ='".$tp->type_id."'>".$tp->type_name."</option>";
+            if($tp->type_id == $typeid )
+            {
+                echo "<option selected value ='".$tp->type_id."'>".$tp->type_name."</option>";
+            }
+            else
+            {
+                echo "<option value ='".$tp->type_id."'>".$tp->type_name."</option>";
+            }
+
+        }
+    }
+    public function getBooktypeadd($id)
+    {
+        $booktype = bt_type::where('category_id',$id)->get();
+        foreach ($booktype as $tp)
+        {
+                echo "<option value ='".$tp->type_id."'>".$tp->type_name."</option>";
         }
     }
 }
