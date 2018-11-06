c5802c01eaff2a1c49c06b67e88097924e6bfd7d
hoaanhvu <44601153+hoaanhvu@users.noreply.github.com>
2018-11-06 10:08:20 +0700

    Update database/migrations/2018_11_06_024720_create_bt_bill_info.php

---
+++ database/migrations/2018_11_06_024720_create_bt_bill_info.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBtBillInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bt_billsinfo', function (Blueprint $table) {
+            $table->increments('billinfo_id');
+            $table->integer('bill_id');
+            $table->integer('book_id');
+            $table->integer('book_price');
+            $table->integer('book_quantity');
+            $table->integer('book_sale');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
