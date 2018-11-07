d5816c4e58925e7ed2c100ceb7a434ce559cad6e
hieupd <phamdanghieu47@gmail.com>
2018-11-07 16:01:59 +0700

    hieupd

--- app/Http/Middleware/CheckPermison.php
+++ app/Http/Middleware/CheckPermison.php
@@ -9,10 +9,10 @@ class CheckPermison
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
+        * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @return mixed
+        */
     public function handle($request, Closure $next)
     {
         if(!Auth::check()){
